4fc9e7bb668abc7236d2a9a4d4c9058ae38aa23f
LarveTable <cyorick@gmail.com>
2026-03-24 18:41:38 +0100

    user and refreshtoken tables

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,15 @@
+import 'dotenv/config'
 import Fastify from 'fastify'
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from './generated/prisma/client.js'
 
 const app = Fastify({ logger: true })
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
 
 app.get('/health', async () => {
-  return { status: 'ok' }
+  const userCount = await prisma.user.count()
+  return { status: 'ok', users: userCount }
 })
 
 app.listen({ port: 3000 }, (err) => {
